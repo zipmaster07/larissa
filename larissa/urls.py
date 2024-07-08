0efc9f35c539ca545d10596240329d4a172527dd
Daniel Schaeffer <dschaeffer@mpb-europa.local>
2024-07-07 21:49:27 -0600

    Update root and app urls.py files

--- larissa/urls.py
+++ larissa/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('scoring/', include('scoring.urls')),
+    path('', include('scoring.urls')),
     path('admin/', admin.site.urls),
 ]
